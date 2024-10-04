c320f55d3d4d42a5727d0aa0cae8dac6e66fc4e6
Maurice Gerczuk <maurice.gerczuk@uni-a.de>
2024-10-04 12:16:20 +0200

    Chase module updates in templates.

--- templates/dl-python/gpujob.sh
+++ templates/dl-python/gpujob.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH --time=00:10:00
-#SBATCH --partition=students
+#SBATCH --partition=dfl
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=5G
 #SBATCH --output=./slurm.out
 
-# This activates the environment (given .envrc is in the current directory)
 # This activates the environment (given .envrc is in the current directory)
 # If you do not want to have the .envrc in your current directory, use this snippet
 # pushd $(pwd)
